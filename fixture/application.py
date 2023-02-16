76c767e17107f51ac3d814c620846cec49b4cf3f
Igor <shakhov96@mail.ru>
2023-02-17 00:38:19 +0300

    class for management session

--- fixture/application.py
+++ fixture/application.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-
+from fixture.session import SessionHelper
 
 class Application:
 
@@ -11,21 +11,13 @@ class Application:
         s = Service('/drivers/chromedriver.exe')
         self.wd = webdriver.Chrome(service=s)
         self.wd.implicitly_wait(60)
+        self.session = SessionHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost:8080/addressbook/")
 
 
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element("name", "user").click()
-        wd.find_element("name", "user").clear()
-        wd.find_element("name", "user").send_keys(username)
-        wd.find_element("name", "pass").clear()
-        wd.find_element("name", "pass").send_keys(password)
-        wd.find_element("id", "LoginForm").submit()
 
     def open_groups_page(self):
         wd = self.wd
@@ -81,9 +73,6 @@ class Application:
         wd = self.wd
         wd.find_element(By.LINK_TEXT, "home").click()
 
-    def logout(self):
-        wd = self.wd
-        wd.find_element(By.LINK_TEXT, "Logout").click()
 
     def destroy(self):
         self.wd.quit()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -9,12 +9,12 @@ def app(request):
     return fixture
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(name="user1", company="it", address="USA"))
-    app.logout()
+    app.session.logout()
 
 def test_add_empty_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(name="", company="", address=""))
-    app.logout()
+    app.session.logout()
 
