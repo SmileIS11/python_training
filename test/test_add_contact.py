ade12306d7075736890227f985de585811641c6e
Igor <shakhov96@mail.ru>
2023-02-21 08:30:29 +0300

    validation

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,12 +3,10 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(name="user1", company="it", address="USA"))
-    app.session.logout()
+
 
 def test_add_empty_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(name="", company="", address=""))
-    app.session.logout()
+
 
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,12 +5,10 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="group123", header="afafasf", footer="aSFAF"))
-    app.session.logout()
+
 
 
 def test_add_empty_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
+
